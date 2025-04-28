a2c5e659af3e4f0dc2ff18d239904806eca67708
Samuel Alingasa Cabagay <34829671+Samuelcabz@users.noreply.github.com>
2025-04-29 03:55:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -132,20 +132,6 @@ def login_and_click_button():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--log-level=3")
-    options.page_load_strategy = 'eager'
-
-    # Block unnecessary resources
-    prefs = {
-        'profile.managed_default_content_settings.images': 2,
-        'profile.managed_default_content_settings.stylesheets': 2,
-        'profile.managed_default_content_settings.fonts': 2,
-        'profile.managed_default_content_settings.media_stream': 2,
-        'profile.managed_default_content_settings.notifications': 2,
-    }
-    options.add_experimental_option('prefs', prefs)
     options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})    
 
  
