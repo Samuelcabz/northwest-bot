88724a80dbc489c37b39eef15337d749fb384043
Samuel Alingasa Cabagay <34829671+Samuelcabz@users.noreply.github.com>
2025-05-11 04:17:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import requests
 url = "https://relyhome.com/login/"
 apikey = 'ccf7183648c4316217ed45e5a11c78a5'  # 2Captcha API key 
 solver = TwoCaptcha(apikey, pollingInterval=1)
+ACCOUNT_EMAIL = "FL-NorthWest@FidelisRepairs.com"
 
 # LOCATORS
 signin_button = "//button[@type='submit']"
@@ -71,6 +72,7 @@ def send_email_notification_to_me(subject, body):
 def send_job_to_api(system, location, day, time_slot,url,swo):
     try:
         response = requests.post("https://bot101.pythonanywhere.com/api/jobs/store", json={
+            "account": ACCOUNT_EMAIL,
             "system": system,
             "location": location,
             "day": day,
